2cca66bb8c459b397bd2b3f5881db0f86e0461a6
Steven Shaginyan <steven.shaginyan@heroku.com>
2018-03-19 16:18:43 -0700

    12th commit

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -2,7 +2,7 @@
 # ../node_modules/.bin/sequelize db:migrate
 # ../node_modules/.bin/sequelize db:seed:all
 
-cd ../react-ui/
+cd react-ui/
 npm install &&
 npm install --only=dev --no-shrinkwrap &&
 npm run build
