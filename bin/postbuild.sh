89b6879e9838a480dc6d637daee10e9e5639027d
Steven Shaginyan <steven.shaginyan@heroku.com>
2018-03-19 16:16:30 -0700

    11th commit

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -1,6 +1,6 @@
-cd server/
-../node_modules/.bin/sequelize db:migrate
-../node_modules/.bin/sequelize db:seed:all
+# cd server/
+# ../node_modules/.bin/sequelize db:migrate
+# ../node_modules/.bin/sequelize db:seed:all
 
 cd ../react-ui/
 npm install &&
